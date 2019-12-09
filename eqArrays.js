22ade540381039bea31eee080e67c3848d3099c0
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-12-08 21:04:35 -0700

    create assertion for equal arrays

---
+++ eqArrays.js
@@ -0,0 +1,19 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+}
+
+const eqArrays = (first, second) => {
+  for (let i in first) {
+    if (first[i] !== second[i]) return false
+  }
+  return true
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
