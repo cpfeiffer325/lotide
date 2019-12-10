25d4ad64f0932e07a361d7e60fecc602531bb61c
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-12-09 23:25:23 -0700

    refactor password obfuscator function

---
+++ passwordObfuscator.js
@@ -0,0 +1,11 @@
+let myArgs = process.argv;
+
+let obfuscatePassword = function(password) {
+  let newPassword = password.replace(/a/g, "4")
+  newPassword = newPassword.replace(/e/g, "3")
+  newPassword = newPassword.replace(/o/g, "0")
+  newPassword = newPassword.replace(/l/g, "1")
+  return newPassword
+}
+
+console.log(obfuscatePassword(myArgs[2]))
