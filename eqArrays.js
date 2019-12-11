981f293baac50e9914ea7cb50c097b0a46b6537a
cpfeiffer325 <christian.pfeiffer@payload.com>
2019-12-11 11:22:38 -0700

    clean up code

--- eqArrays.js
+++ eqArrays.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual')
-
 const eqArrays = (first, second) => {
   for (let i in first) {
     if (first[i] !== second[i]) return false
@@ -8,8 +6,3 @@ const eqArrays = (first, second) => {
 }
 
 module.exports = eqArrays
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true)
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false)
