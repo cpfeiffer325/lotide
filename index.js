eb3183708193277b5e4221ec49e43d66201bf712
cpfeiffer325 <christian.pfeiffer@payload.com>
2019-12-10 22:54:01 -0700

    create index file to reference all utility functions

---
+++ index.js
@@ -0,0 +1,27 @@
+const assertArraysEqual = require('./assertArraysEqual')
+const assertEqual = require('./assertEqual')
+const countLetter = require('./countLetter')
+const countOnly = require('./countOnly')
+const eqArrays = require('./eqArrays')
+const flatten = require('./flatten')
+const head   = require('./head')
+const joinConcepts   = require('./joinConcepts')
+const middle = require('./middle')
+const passwordObfuscator = require('./passwordObfuscator')
+const tail   = require('./tail')
+const without = require('./without')
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  countLetter: countLetter,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  flatten: flatten,
+  head: head,
+  joinConcepts: joinConcepts,
+  middle: middle,
+  passwordObfuscator: passwordObfuscator,
+  tail: tail,
+  without: without
+}
