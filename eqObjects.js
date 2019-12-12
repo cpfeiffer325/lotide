d3b36d2da1d1b5fe0203ba3d0e81adff0786db42
Christian Pfeiffer <cpfeiffer325@gmail.com>
2019-12-11 23:05:06 -0700

    equal objects function and test

--- eqObjects.js
+++ eqObjects.js
@@ -1,9 +1,12 @@
-const eqArrays = require('../eqArrays')
+const eqArrays = require('./eqArrays')
 
-const eqObjects = function(object1, object2, isArray) {
+const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length === Object.keys(object2).length) {
-    for (key of Object.keys(object1)) {
-      if (object1[key] === object2[key]) {
+    let keys = Object.keys(object1)
+    for (key of keys) {
+      if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+        eqArrays(object1[key], object2[key])
+      } else if (object1[key] === object2[key]) {
         return true
       }
     }
